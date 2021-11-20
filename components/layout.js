14c6f8616f6f6288f8b478f2e635cdcf6c2a6fda
notaspecial <notaspecial@gmail.com>
2021-11-20 21:51:39 +0700

    push

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Adamrah'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/adam.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -39,12 +39,12 @@ export default function Layout({ children, home }) {
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
-          <>
+          <> 
             <Link href="/">
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/adam.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
